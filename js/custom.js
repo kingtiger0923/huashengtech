1e44aa26d49c08717c465b2506b642c231ffa48a
stacking42 <suda1125@outlook.com>
2020-04-07 21:57:38 +0800

    update

--- js/custom.js
+++ js/custom.js
@@ -144,7 +144,7 @@ $(document).ready(function(){
         $(".number-counters [data-to]").each(function () {
             var e = $(this).attr("data-to");
             $(this).delay(6e3).countTo({
-                from: 50,
+                from: 0,
                 to: e,
                 speed: 3e3,
                 refreshInterval: 50
